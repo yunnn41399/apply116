ed9f96ac70b936a621960dd623f53553c63105c6
yunnn41399 <615415036@alum.ccu.edu.tw>
2026-08-11 21:34:53 +0800

    Create login.php

---
+++ app/Views/Login/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>考生登入</title>
+</head>
+
+<body>
+    <h1>考生登入</h1>
+    <h3>學測應試號碼</h3>
+    <h3>身分證號碼</h3>
+    <h3>密碼</h3>
+    <h3>驗證碼</h3>
+</body>
