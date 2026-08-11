eafbdf88b4b34cdc8fde92f791cca417108bc50b
Ying-Yu, Chien <luya.chien@gmail.com>
2026-08-11 16:47:51 +0800

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -4,3 +4,5 @@ use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
 $routes->get('/', 'Home::index');
+$routes->get('/register', 'Register::index');
+$routes->post('/register', 'Register::register');
