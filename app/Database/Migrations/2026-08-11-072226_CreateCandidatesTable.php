d594d32ae5659e65dba56ac777469907434e9fb2
Ying-Yu, Chien <luya.chien@gmail.com>
2026-08-11 16:37:40 +0800

    Update candidate database migration

--- app/Database/Migrations/2026-08-11-072226_CreateCandidatesTable.php
+++ app/Database/Migrations/2026-08-11-072226_CreateCandidatesTable.php
@@ -8,11 +8,43 @@ class CreateCandidatesTable extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INTEGER',
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'exam_number' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 20,
+            ],
+            'id_number' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 20,
+            ],
+            'password' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->addUniqueKey('exam_number');
+        $this->forge->addUniqueKey('id_number');
+
+        $this->forge->createTable('candidates');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('candidates');
     }
 }
