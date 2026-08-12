04ecc07fe895c9f80fb4b04c258a3ea0f33a49e3
Ying-Yu, Chien <luya.chien@gmail.com>
2026-08-12 16:04:11 +0800

    Add registration success page

---
+++ app/Views/register_success.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+    <meta charset="UTF-8">
+    <title>註冊完成</title>
+</head>
+<body>
+
+    <h1>註冊完成</h1>
+
+    <p>恭喜您，考生帳號已註冊成功！</p>
+
+    <p>請使用您的學測應試號碼、身分證號碼及個人密碼登入網路報名系統。</p>
+
+</body>
+</html>
