ba3f01697aad9006b7ee7b4a3e39c0d7f634bb71
yunnn41399 <615415036@alum.ccu.edu.tw>
2026-08-13 21:03:34 +0800

    Create PasswordController.php

---
+++ app/Controllers/PasswordController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class PasswordController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
