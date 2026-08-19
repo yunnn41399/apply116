f571e3dbe6ff9cf76cededb3957304b12d6845af
Ying-Yu, Chien <luya.chien@gmail.com>
2026-08-19 14:32:35 +0800

    Update candidates and application page routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,62 +7,45 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 
-// 管理員登入
+// 管理員登入／登出
 $routes->get('admin/login', 'AdminAuth::login');
 $routes->post('admin/login', 'AdminAuth::attemptLogin');
 $routes->post('admin/logout', 'AdminAuth::logout');
 
-$routes->get('/admin', 'Admin::index');
 
-$routes->get('/admin/candidates', 'CandidateAdmin::index');
-$routes->get('/admin/candidates/(:num)', 'CandidateAdmin::detail/$1');
+// 後臺管理功能，必須登入管理員帳號才能進入頁面
+$routes->group('admin', ['filter' => 'adminAuth'], function ($routes) {
 
-$routes->get('/admin/applications', 'CandidateApplicationAdmin::index');
+    // 後臺首頁
+    $routes->get('/', 'Admin::index');
 
-$routes->get('/admin/applications', 'ApplicationAdmin::index');
-$routes->get('/admin/application/(:num)', 'ApplicationAdmin::detail/$1');
+
+    // 考生資料
+    $routes->get('candidates', 'CandidateAdmin::index');
+    $routes->get('candidates/(:num)', 'CandidateAdmin::detail/$1');
+
+
+    // 報名資料
+    $routes->get('applications', 'ApplicationAdmin::index');
+    $routes->get('application/(:num)', 'ApplicationAdmin::detail/$1');
+
+
+    // 公告管理
+    $routes->get('announcement', 'Announcement::adminIndex');
+    
+    $routes->get('announcement/create', 'Announcement::create');
+    $routes->post('announcement/create', 'Announcement::create');
+
+    $routes->get('announcement/edit/(:num)', 'Announcement::edit/$1');
+    $routes->post('announcement/edit/(:num)', 'Announcement::edit/$1');
+
+    $routes->post('announcement/delete/(:num)', 'Announcement::delete/$1');
+});
+
 
 // 前台公告
 $routes->get('/announcement', 'Announcement::index');
 $routes->get('/announcement/(:num)', 'Announcement::detail/$1');
-
-
-// 後臺公告管理
-$routes->get(
-    '/admin/announcement',
-    'Announcement::adminIndex',
-    ['filter' => 'adminAuth']
-);
-
-$routes->get(
-    'admin/announcement/create',
-    'Announcement::create',
-    ['filter' => 'adminAuth']
-);
-
-$routes->post(
-    'admin/announcement/create',
-    'Announcement::create',
-    ['filter' => 'adminAuth']
-);
-
-$routes->get(
-    '/admin/announcement/edit/(:num)',
-    'Announcement::edit/$1',
-    ['filter' => 'adminAuth']
-);
-
-$routes->post(
-    '/admin/announcement/edit/(:num)',
-    'Announcement::edit/$1',
-    ['filter' => 'adminAuth']
-);
-
-$routes->post(
-    '/admin/announcement/delete/(:num)',
-    'Announcement::delete/$1',
-    ['filter' => 'adminAuth']
-);
 
 
 // 考生註冊
@@ -73,22 +56,26 @@ $routes->post('/register', 'Register::register');
 // 考生登入
 $routes->get('/login', 'LoginController::index');
 $routes->post('/login', 'LoginController::login');
+
 $routes->get('/login/refresh-captcha', 'LoginController::refreshCaptcha');
+
 $routes->get('/logout', 'LoginController::logout');
 
 
 // 忘記密碼
 $routes->get('/forgot-password', 'PasswordController::forgot');
 $routes->post('/forgot-password/verify', 'PasswordController::verify');
+
 $routes->get('/reset-password', 'PasswordController::reset');
 $routes->post('/reset-password/update', 'PasswordController::update');
 
 
 // 報名系統
 $routes->get('/apply', 'ApplyController::index');
-$routes->get('/department', 'DepartmentController::index');
-$routes->get('/application', 'ApplicationController::index');
 
+$routes->get('/department', 'DepartmentController::index');
+
+$routes->get('/application', 'ApplicationController::index');
 $routes->post('/application/save', 'ApplicationController::save');
 
 
