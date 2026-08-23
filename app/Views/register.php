a90778b51aea4a1cbcd4e6f2b8e03d0b8e7c8264
Ying-Yu, Chien <luya.chien@gmail.com>
2026-08-23 21:29:52 +0800

    Add hyperlink to title

--- app/Views/register.php
+++ app/Views/register.php
@@ -20,7 +20,9 @@
 
     <!-- 網站標題列 -->
     <header class="page-header">
-        <h1>Apply116</h1>
+        <a href="<?= base_url('/') ?>" class="page-header-link">
+            <h1>Apply 116</h1>
+        </a>
     </header>
 
     <main class="form-container register-container">
